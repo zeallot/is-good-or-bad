a8107d13cacbef5bc72af2494ea9d0b6777320e9
vkaboskin <vkaboskin@aspirity.com>
2020-09-15 21:19:23 +0700

    add more function

--- index.js
+++ index.js
@@ -7,7 +7,20 @@ function getRandomItemFromArray (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-export default function isGood(something) {
+function getTrueOrFalse() {
+  const True = true;
+  const False = false;
+  getRandomItemFromArray([True, False]);
+}
+
+export default function isGoodOrBad(something) {
   return `${something} ${getRandomItemFromArray(options)}`;
 };
 
+export function isGood(something) {
+  return !isBad(something);
+}
+
+export function isBad(something) {
+  return getTrueOrFalse();
+}
